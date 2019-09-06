c7e8a661a28e7cbc7a938332ae5fac901d2579b9
SamanthaLossett <samanthalossett@gmail.com>
2019-09-05 18:13:49 -0700

    connected to Mongo successfully

--- test/connection.js
+++ test/connection.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'connection error: SAM FREAK OUT NOW'));
 db.once('open', function() {
+    console.log("YOU'RE CONNECTED TO MONGO SAM! whoop!");
   // we're connected!
 });
